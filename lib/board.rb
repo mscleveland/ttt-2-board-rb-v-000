fbd6aa6f1414ecb1b867bf5ad66894cd14197e52
Erin Shank <emshank@gmail.com>
2017-11-28 17:04:12 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,34 @@
+row = [" ", " ", " "]
+
+# Position 1: Left
+row[0]
+
+# Position 2: Middle
+row[1]
+
+# Position 3: Right 
+row[2]
+
+row = [" ", " ", " "]
+
+# Position 4: Left
+row[3]
+
+# Position 5: Middle
+row[4]
+
+# Position 6: Right
+row[5]
+
+row = [" ", " ", " "]
+
+# Position 7: Left 
+row[6]
+
+#Position 8: Middle
+row[7]
+
+#Position 9: Right
+row[8]
+
 # Define the variable board below.
